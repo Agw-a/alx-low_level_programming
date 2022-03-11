fbed98b86af730d92360355aa568f1fe913e9a81
Agw-a <lizagwa7@gmail.com>
2022-03-10 18:23:58 -0800

    Adding 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
-/*main-c entry point
- * Return-always o*/
+#include<stdio.h>
 
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
