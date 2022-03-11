2a2407f84cf107a797ab8b333f44a26b6b440652
Agw-a <lizagwa7@gmail.com>
2022-03-10 18:44:22 -0800

    nwew

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * return: always 0 (Sucess)
+ * main - prints with proper grammar, but the outcome is a piece of art using printf.
  */
 int main(void)
 
