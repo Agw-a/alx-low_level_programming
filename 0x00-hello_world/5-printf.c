5fbcbb2348ae936ca7e00178b5e526b04e768450
Agw-a <lizagwa7@gmail.com>
2022-03-10 18:38:08 -0800

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * return:Always 0
+ */
 int main(void)
-/*main-c entry point                                                                                                                    
- *Return-o*/
+
 {printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
