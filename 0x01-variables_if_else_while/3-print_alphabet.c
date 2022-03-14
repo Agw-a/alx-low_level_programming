e89ef3471b18833b8950fed6d466f6ce56f5d3cc
Agw-a <lizagwa7@gmail.com>
2022-03-14 03:33:36 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph[52] = "abcdefghijklmopqrstuvxyzABCCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for ( i = 0 ; i < 52; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
