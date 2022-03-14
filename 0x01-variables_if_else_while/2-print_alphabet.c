5b09471305b430652cc2ad3db9a1eafdf4cd6792
Agw-a <lizagwa7@gmail.com>
2022-03-14 02:54:03 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,35 +3,25 @@
 
 
 /**
- *
- *  * main - Prints the alphabet.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+  * main - Prints the alphabet.
+  *
+  * Return: Always 0 (Success)
+  */
 
 int main(void)
 
 {
 
-		char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
 
-			int i;
+for (i = 0; i < 26; i++)
+{
+	putchar(alph[i]);
 
+}
 
+putchar('\n');
 
-				for (i = 0; i < 26; i++)
-
-						{
-
-									putchar(alp[i]);
-
-										}
-
-					putchar('\n');
-
-						return (0);
-
+return (0);
 }
