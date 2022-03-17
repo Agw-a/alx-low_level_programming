75708a30ff30d64f55725d7e9c62a57a77b81409
Agw-a <lizagwa7@gmail.com>
2022-03-17 01:56:14 -0700

    Main files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main-testa function tghat prints if the integer is positive or negative.
+ * Return : 0 Success
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
