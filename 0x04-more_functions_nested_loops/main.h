d803826db1427e4cfd2ead2281a829bb1af190c7
Agw-a <lizagwa7@gmail.com>
2022-03-17 09:05:39 -0700

    new

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,5 +5,3 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-
-endif /* MAIN_H */
