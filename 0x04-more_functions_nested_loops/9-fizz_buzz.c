59f33957b4725cf30af7b30100ecd7ca4b0297a8
Agw-a <lizagwa7@gmail.com>
2022-03-17 12:14:30 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,10 +10,10 @@ int main(void)
 
 	n = 1;
 	printf("%d", n);
-	for (n >= 2; n <= 100; n++)
-	{
+	for (n = 2; n <= 100; n++)
 	{
 	if ((n % 3 == 0) && (n % 5 == 0))
+	{
 	printf(" FizzBuzz");
 	}
 	else if (n % 3 == 0)
