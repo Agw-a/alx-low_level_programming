1b4bf3800b73aa2b5bc4aaa20f99a4d335ac2f79
Agw-a <lizagwa7@gmail.com>
2022-03-17 13:09:42 -0700

    Describing parameters in comments

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isdigit - checks if parameter is a number between 0 to 
+ * _isdigit - check if parameter is a number between 0 to 9. 
  * @c:input number
  * Return: 1 if a number (0-9), 0 if other case.
  */
