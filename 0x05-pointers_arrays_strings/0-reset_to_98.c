bdbf0e6a28c27c35f913ca6b9aace064003c6aa6
Agw-a <lizagwa7@gmail.com>
2022-03-21 04:33:44 -0700

    Betty style coding:

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *void reset_to_98 -  takes a parameter and updates the value
+ *reset_to_98 -  takes a parameter and updates the value
  *@n: integer
  *Return:no return
  */
