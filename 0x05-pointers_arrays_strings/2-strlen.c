8c017536835318291905a8068f768590a0475084
Agw-a <lizagwa7@gmail.com>
2022-03-21 08:05:39 -0700

    Adding comments

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 
 /**
  *_strlen - returns the length of a string.
- *@s:String
- *Return:count
+ *@s:input string
+ *Return:string length
  */
 int _strlen(char *s)
 {
 	int count = 0;
 
-	while (*(s + count) != '\0');
+	while (*(s + count) != '\0')
 	count++;
 	return (count);
 }
