cae1f5557a2d00eff3b8078744db7c5792a49732
Agw-a <lizagwa7@gmail.com>
2022-03-21 06:14:35 -0700

    String

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string.
+ *@a:First integer
+ *@b:Second integer
+ *Return:no return
+ */
+int _strlen(char *s)
+{
+	int c = *a;
+
+	while (*(s + count) != '\0');
+	count++;
+	return (count);
+}
