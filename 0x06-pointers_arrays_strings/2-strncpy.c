27abaa81abf299b764602f2c8bc168d00cc105e6
Agw-a <lizagwa7@gmail.com>
2022-03-24 08:26:00 -0700

    Removing whitespace

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strncpy -  copiess a string
- * @dest: destination
- * @src: source
- * @n: bytes from src
- * return: pointer to dest
+ * _strncpy-copiess a string
+ * @dest:destination
+ * @src:source
+ * @n:bytes from src
+ * return:pointer to dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
