5421cf3c93907d69de9d5586f1481885ded22e76
Agw-a <lizagwa7@gmail.com>
2022-03-24 06:47:58 -0700

    Adding task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncpy -  copies a string
+ * _strncpy -  copiess a string
  * @dest: destination
  * @src: source
  * @n: bytes from src
@@ -11,9 +11,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	dest[i] = src[i];
-	for (; i < n; i++)
+	for ( ; i < n; i++)
 	dest[i] = '\0';
+
 	return (dest);
 }
