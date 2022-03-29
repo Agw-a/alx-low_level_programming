a4dcdd070a9b129b7f4904dd4ca393a85c664fef
Agw-a <lizagwa7@gmail.com>
2022-03-29 06:57:11 -0700

    TYask 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,13 +7,11 @@
  * @n:bytes filled
  * return:pointer to dest
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
-	*(dest + i) = *(src + i);
-	}
+	*(dest + i) =  *(src + i);
 	return (dest);
 }
