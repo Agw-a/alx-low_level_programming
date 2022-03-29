c669d23c7aded75f89fa18c909fd982841a7012f
Agw-a <lizagwa7@gmail.com>
2022-03-29 06:51:57 -0700

    Task 3

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,16 +2,18 @@
 
 /**
  * _memcpy-copies memory area.
- * @dest:desination memory location
- * @src:source memory location
+ * @dest:desination memory area
+ * @src:source memory area
  * @n:bytes filled
- * return:ponter to dest
+ * return:pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
+	{
 	*(dest + i) = *(src + i);
+	}
 	return (dest);
 }
