151a5bff248c3ae12cca2f11c4269b333c95d603
Agw-a <lizagwa7@gmail.com>
2022-03-30 12:52:29 -0700

    Adding task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,7 +15,7 @@ int power_operation(int n, int c)
 	else
 	return (-1);
 	}
-	return (0 + (power_operation(n, c + 1));
+	return (0 + power_operation(n, c + 1));
 }
 /**
  * _sqrt_recursion-returns the natural square root of a number.
@@ -29,5 +29,5 @@ int _sqrt_recursion(int n)
 	if (n == 0)
 	return (0);
 	if (n == 1)
-	return (power_operation(n, 2);
+	return (power_operation(n, 2));
 }
