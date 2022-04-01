aa3b7e8925d74018254827cef49de2f8c3e51b81
Agw-a <lizagwa7@gmail.com>
2022-04-01 15:20:24 -0700

    adding task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,10 @@
  * main-prinmts its name followed by new line.
  * @argc:number of command line arguments.
  * @argv:arrays that contains command line arguments.
- * Return:0 success.
+ * Return:0 - success.
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return(0);
+	return (0);
 }
