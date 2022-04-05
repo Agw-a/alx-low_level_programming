f46d5fee2321516472964d78e205c770864f223e
Agw-a <lizagwa7@gmail.com>
2022-04-05 04:49:36 -0700

    Tsk 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,15 +13,15 @@ char *str_concat(char *s1, char *s2)
 	char *stropt;
 	unsigned int i, j, k, limit;
 
-	if (s1 = NULL)
+	if (s1 == NULL)
 	s1 = "";
-	if (s2 = NULL)
+	if (s2 == NULL)
 	s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	;
 	for (j = 0; s2[j] != '\0'; j++)
 	;
-	stropt = (malloc(sizeof(char) * (i + j + 1));
+	stropt = malloc(sizeof(char) * (i + j + 1));
 	if (stropt == NULL)
 	{
 	free (stropt);
