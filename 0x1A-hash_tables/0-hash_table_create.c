b22e47a8e59430aa5f3a7a99eb5a6edfa5ba38b9
Agw-a <lizagwa7@gmail.com>
2022-07-01 14:08:41 -0700

    memory fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,14 +13,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	h_table = malloc(sizeof(hash_table_t));
 	if (!h_table)
 		return (NULL);
+	if (size == NULL)
+		return (h_table);
 
-	h_table->size = size;
 
 	h_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (h_table->array == NULL)
+	if (!h_table->array)
 	{
-		free(h_table->array);
 		return (NULL);
 	}
+	h_table->size = size;
 	return (h_table);
 }
