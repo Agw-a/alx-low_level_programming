b6d8b72ba751b138a9d46d8cdd97499a478108cb
Agw-a <lizagwa7@gmail.com>
2022-08-17 13:45:52 -0700

    Tech Int Prep

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Module that returns perimeret of island
+    """
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row - 1 < 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if col - 1 < 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col + 1 >= len(grid[row]) or grid[row][col + 1] == 0:
+                    perimeter += 1
+                if row + 1 >= len(grid) or grid[row + 1][col] == 0:
+                    perimeter += 1
+    return perimeter
