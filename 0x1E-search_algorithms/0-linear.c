96d027453170cf11d071230a3881d45f26050fa4
Agw-a <lizagwa7@gmail.com>
2022-10-04 02:34:28 -0700

    Task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+/**
+ * linear_search- searches for a value in an array of integer
+ *
+ * @array:pointer to the first element of the array
+ * @size:number of elements
+ * @value:value to search for
+ * Return:first index of value of -1 if it does not exist
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCHALGOS_H_
+#define SEARCHALGOS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+int linear_search(int *array, size_t size, int value);
+
+
+#endif /*SEARCHALGOS_H_*/
