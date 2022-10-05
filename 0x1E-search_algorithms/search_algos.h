c5875486ea38c724a92646e46564178e24fe599f
Agw-a <lizagwa7@gmail.com>
2022-10-05 12:09:32 -0700

    ___:

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
-int recursiveBinarySearch(int *array, size_t first_idx, size_t last_idx, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int inter(int *array, size_t low, size_t high, size_t size, int value);
