e276bc790bd768078b2dc4f61b1634fc4a64eb00
Agw-a <lizagwa7@gmail.com>
2022-10-05 09:47:08 -0700

    Interpolate

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+
+/**
+ * interpolate-recursively interpolate through array(search)
+ * @array:array of values
+ * @low:first index in array
+ * @high:last index in array
+ * @size:size of array
+ * @value:value to locate in array
+ * Return:index of value.
+ */
+int interpolate(int *array, size_t low, size_t high, size_t size, int value)
+{
+	size_t pos;
+	int x;
+
+	pos = low + (((double)(high - low) / (array[high] - array[low])) *
+			(value - array[low]));
+	if (pos >= size)
+	{
+		printf("Value checked array[%ld] is out of range\n", pos);
+		return (-1);
+	}
+	printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+	if (value == array[pos])
+		return (pos);
+	else if (value > array[pos])
+		x = inter(array, pos + 1, high, size, value);
+	else if (value < array[pos])
+		x = inter(array, low, pos - 1, size, value);
+	return (x);
+}
+
+/**
+ * interpolation_search-searches for a value in a sorted array of integers
+ * @array:pointer to the first element of the array to search
+ * @size:number of elements in array
+ * @value:the value to search
+ * Return:index of value or -1 on failure
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	if (!array)
+		return (-1);
+	return (inter(array, 0, size - 1, size, value));
+}
